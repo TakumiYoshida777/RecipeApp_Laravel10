2f2d20cbf63cde0ae44399271125f462479ae0fd
takumi <yoshida.fm.0626@gmail.com>
2023-12-17 10:19:38 +0900

    手順の追加削除機能　材料の追加削除機能　実装

--- public/js/recipe/create.js
+++ public/js/recipe/create.js
@@ -3,10 +3,13 @@
 const stepNo = document.querySelectorAll(".stepNo");
 
 const steps = document.getElementById('steps');
+const ingredients = document.getElementById('ingredients');
 const stepDeleteIcons = document.querySelectorAll(".step-delete");
 const stepAddIcon = document.querySelector(".step-add");
+const ingredientAddIcon = document.querySelector(".ingredient-add");
 const oneStepRecord = document.querySelector(".step");
-console.log(oneStepRecord, "oneStepRecord");
+const ingredientItem = document.querySelector(".ingredient-item");
+
 
 
 /**
@@ -19,33 +22,95 @@ const sortNomber = () => {
     });
 }
 
-/**
- * 削除する
- * @param {} e
- */
-const handleStepDelete = (e) => {
-    e.target.closest('.step').remove();
-    sortNomber();
+//材料のrecordのインデックスを順番に振りなおす
+const indexRestartIngredent = () => {
+    const items = ingredients.querySelectorAll('.ingredient-item');
+    items.forEach(function(item, index) {
+        item.querySelector('.ingredient-name').name = `ingredients[${index}][name]`;
+        item.querySelector('.ingredient-quantity').name = `ingredients[${index}][quantity]`;
+    });
 }
 
-const sortable = Sortable.create(steps, {
+
+/**
+ * ステップを削除する
+ * @param {} e
+ */
+const handleRecodeDelete = (e) => {
+    if(e.target.closest('.step')){
+        e.target.closest('.step').remove();
+        sortNomber();
+    }
+    else if(e.target.closest('.ingredient-item')){
+        e.target.closest('.ingredient-item').remove();
+        indexRestartIngredent();
+
+    }
+}
+
+/**
+ * stepsをソートする
+ */
+const sortableSteps = Sortable.create(steps, {
     //オプションを入力
     animation: 150,
     handle: '.handle',
     onEnd: sortNomber
 });
 
+/**
+ * ingredientsをソートする
+ */
+const sortableIngredients = Sortable.create(ingredients, {
+    //オプションを入力
+    animation: 150,
+    handle: '.handle',
+    onEnd: indexRestartIngredent
+});
+
 console.log(steps, "steps");
 
-const addStep = (e) => {
-    const addStep = steps.appendChild(oneStepRecord.cloneNode(true));
-    sortNomber();
-    addStep.querySelector('.step-delete').addEventListener('click', handleStepDelete);
+
+
+//行を追加する
+const addRecord = (e) => {
+    //ステップ
+    if(e.target.closest('.step-add')){
+        const addRecord = steps.appendChild(oneStepRecord.cloneNode(true));
+        sortNomber();
+        addRecord.querySelector('.step-delete').addEventListener('click',handleRecodeDelete);
+    }
+    //材料
+    else if(e.target.closest('.ingredient-add')){
+        // const newIngredientItem =ingredientItem.value="";
+        console.log(ingredientItem)
+        const addRecord = ingredients.appendChild(ingredientItem.cloneNode(true));
+        sortNomber();
+        addRecord.querySelector('.step-delete').addEventListener('click',handleRecodeDelete);
+        indexRestartIngredent();
+    }
 }
 
-stepAddIcon.addEventListener('click', (e) => addStep());
+/**
+ * ステップのプラスボタン押下処理
+ */
+stepAddIcon.addEventListener('click',addRecord);
 
+/**
+ * 材料のプラスボタン押下処理
+ */
+ingredientAddIcon.addEventListener('click',(e) => {
+    console.log(e.target.closest('.ingredient-add'), "材料のプラスボタン押下処理");
+    addRecord(e);
+    indexRestartIngredent();
+});
 
+/**
+ * 削除ボタン押下処理
+ */
 stepDeleteIcons.forEach((item) => {
-    item.addEventListener('click', handleStepDelete);
+    item.addEventListener('click',handleRecodeDelete);
 })
+
+
+//insert時のrecordのインデックスの振り直し処理
